0394318acdba358ddc93db3a81eab169087f1547
Michael Shaughnessy <NoisomePossum@users.noreply.github.com>
2017-03-15 16:30:15 +0100

    Add remaining basic features
    This adds all the appearance plus basic framework of the program.

--- Quotation_generator/js/quote_generate.js
+++ Quotation_generator/js/quote_generate.js
@@ -1,8 +1,5 @@
-// document.getElementById("QuoteApp").innerHTML = generateQuote();
-
-
+// main function
 function generateQuotes() {
-
 	// clear previously generated text
 	// before generating more
 	document.getElementById("quoteResults").innerHTML = "";
@@ -11,30 +8,75 @@ function generateQuotes() {
 	// and store it in a variable.
 	var numberQuotes = document.getElementById("textinput").value;
 
-	var text = assembleText();
-	
-	insertDivs(numberQuotes, text);
+	// Capture the type of quote selected
+	// and put it in a variable
+	var typeQuote = getRadioVal(document.getElementById('quoteForm'), 'radios');
+
+	// Generate text and insert divs into the page
+	insertDivs(numberQuotes);
 
 }
 
+quoteFrags = [
+	"to live in the moment",
+	"to face the wind",
+	"to reach for the stars",
+	"monkeys fall from trees",
+	"to give a mouse a cookie",
+	"To be or not to be", 
+	"That is the question", 
+	"Ain't it?"
+	
+]
+
+function getRadioVal(form, name) {
+	var val;
+	// get list of radio buttons with specified name
+	var radios = form.elements[name];
+
+	// loop through the list of radio buttons
+	for (var i = 0; i < radios.length; i++) {
+		if (radios[i].checked) {
+			val = radios[i].value;
+			break;
+		}
+	}
+
+	return val;
+}
 
 
 function assembleText() {
-	var fragments = ["to be", "or not", "to be"];
-	var text = fragments[0] + " " + fragments[1] + " " + fragments[2];
+	var text = "";
+	for (var i = 0; i < 3; i++) {
+		var randomIndex = Math.floor((Math.random() * quoteFrags.length));
+		text += quoteFrags[randomIndex] + " ";
+	}
+	// var text = quoteFrags[0] + " " + quoteFrags[1] + " " + quoteFrags[2];
 	return text;
 }
 
 
-function insertDivs (numberQuotes, text) {
+function insertDivs (numberQuotes) {
+
+	if (numberQuotes < 3) {
+		colWidth = 12 / numberQuotes;
+	}
+	else {
+		colWidth = 4;
+	}
+	
 	for (var i = 0; i < numberQuotes; i++) {
+
+		var text = assembleText();
+		// var text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto illo consequatur, quidem tempora hic laborum, ea sapiente quis. Quas nulla laborum distinctio voluptatibus ipsum impedit sequi laboriosam esse, aspernatur fugiat.";
 		
 		var textDiv = document.createElement('div');
 		textDiv.setAttribute('class', "col-md-12 awesomequote");
 		textDiv.append(text);
 		
 		var bootDiv = document.createElement('div')
-		bootDiv.setAttribute('class', "col-md-4");
+		bootDiv.setAttribute('class', "col-sm-12 col-md-" + colWidth);
 		bootDiv.appendChild(textDiv);
 
 		var quoteArray = document.getElementById("quoteResults");
